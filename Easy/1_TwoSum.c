afc3ab57df133c3fb8786da9c99522ecdd336b5c
gabanvi <gabanvi@gmail.com>
2024-10-30 16:27:05 +0000

    update

--- Easy/1_TwoSum.c
+++ Easy/1_TwoSum.c
@@ -5,17 +5,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) 
+int *twoSum(int *nums, int numsSize, int target, int *returnSize)
 {
-    int* result = (int*)malloc(2 * sizeof(int));
-    
-    //Loop to isolate each number one by one
+    int *result = (int *)malloc(2 * sizeof(int));
+
+    // Loop to isolate each number one by one
     for (int i = 0; i < numsSize - 1; i++)
-    { 
-        //Loop to add the isolated number to each number in the array
+    {
+        // Loop to add the isolated number to each number in the array
         for (int j = i + 1; j < numsSize; j++)
         {
-         // if the current combo adds up to the target, we fill in the result array with these 2 numbers
+            // if the current combo adds up to the target, we fill in the result array with these 2 numbers
             if (nums[i] + nums[j] == target)
             {
                 result[0] = i;
@@ -25,33 +25,32 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
             }
         }
     }
-    *returnSize = 0; 
+    *returnSize = 0;
     return (0);
 }
 
-//int main () not to be included in the leetcode solution submission
-int main ()
+// int main () not to be included in the leetcode solution submission
+int main()
 {
-    int nums[] = {2, 7, 11, 15}; // Example 
-    int numsSize = sizeof(nums) / sizeof(nums[0]); //Number of element = Tot size of array in bytes / size of one element in bytes 
-    int target = 9; // Example target
+    int nums[] = {2, 7, 11, 15};                   // Example
+    int numsSize = sizeof(nums) / sizeof(nums[0]); // Number of element = Tot size of array in bytes / size of one element in bytes
+    int target = 9;                                // Example target
     int returnSize;
-    
-    
-    int* result = twoSum(nums, numsSize, target, &returnSize);
-    
+
+    int *result = twoSum(nums, numsSize, target, &returnSize);
+
     // If we find a result
     if (result != NULL)
     {
-        
+
         printf("Numbers that add up to %d: [%d, %d]\n", target, result[0], result[1]);
     }
     else
     {
         printf("No two numbers add up to the target.\n");
     }
-    
+
     free(result);
-    
+
     return 0;
 }
